43a64cb038f08c168b14c47e2b17efeb6b94c2e2
Lukasz Paradylo <lukaszparadylo@gmail.com>
2023-04-13 12:04:52 +0200

    add variable playerMove

---
+++ js/functions.js
@@ -0,0 +1,15 @@
+let computerMove = `kamień`;
+let playerMove = 'papier';
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+
+function printMessage(msg){
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
